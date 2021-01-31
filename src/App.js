4c55f5c098d2a440b1dc7bf824455c159d6077ea
Утбанов Александр <utbanovag@it2g.ru>
2021-01-31 21:47:08 +0500

    ReactRedux part2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Provider } from "react-redux";
 
 import { Profile, LoginForm } from "./components";
+import { Routing } from "./routing/Routing";
 import { store } from "./store";
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <Profile />
-        <LoginForm />
+        <Routing />
       </div>
     </Provider>
   );
